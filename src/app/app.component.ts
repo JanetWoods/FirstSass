c7be130c3fb6cc934d8e5b1f137386e308969edc
Janet Woods <janetnwoods@gmail.com>
2021-06-03 23:50:11 -0400

    working base

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,8 +3,8 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: ['./scss/app.component.scss']
 })
 export class AppComponent {
-  title = 'firstSass';
+  title = "ACME Product Management";
 }
